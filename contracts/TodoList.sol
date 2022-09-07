31671dfdda7c2b19ba30ee3555191d5c0575309e
somya-15 <somyatomar15@gmail.com>
2022-09-07 22:50:12 +0530

    Final Updates

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,12 +1,42 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-//we will keep a track of the number of items in the To-do list with the help of a special variable called state variable 
-	uint public taskCount = 0; //state variables inside of solidity are actaully written to the blockchain. They represent the state of the blockchain and the state of the smart contract is going to change anytime this task count changes. These are a lot like class variables in an object oriented context where the scope of the variable belongs to the entire smart contract, not necessarily like a function or something. 
-	//we can also create a way to read this value from the smart contract with keyword--> public. It provides some function for us that allows us to read the value task count from the to-do list and solidity gives us a function whenever we use this public keyword.
+  uint public taskCount = 0;
 
-//and we will store this value inside the smart contract as a way to get started and ensure that everything is set up properly in our project
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
 
-// we will deploy this simple smart contract to the blockchain and see if we can actually connect to it.
+  mapping(uint => Task) public tasks;
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  constructor() public {
+    createTask("Check out dappuniversity.com");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
 
 }
